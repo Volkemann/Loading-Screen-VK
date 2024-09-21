eceba059ad5c440148ac70f8405b409931f3c1f6
Vkzz <joojaovitor2742@gmail.com>
2024-09-21 14:36:56 -0300

    Tela de carregamento (animated)

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ gsap.fromTo('.loading-page' , {opacity:1},{
     duration:2.5,
     delay:2.7,
 })
+
+window.addEventListener('load', function() {
+    console.log("Página carregada, aguardando antes de remover tela de carregamento.");
+    setTimeout(function() {
+        const loadingPage = document.querySelector('.loading-page');
+        loadingPage.style.display = 'none';
+
+        const container = document.querySelector('.container');
+        container.style.display = 'block';
+    }, 5000); // 5000 milissegundos = 3 segundos
+});
